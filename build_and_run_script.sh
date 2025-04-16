b669ebf27bd359dbe614b809794123789db9b9fe
nefir2 <vadim.ahtamov@gmail.com>
2025-04-16 22:15:50 +0300

    build fixes

--- build_and_run_script.sh
+++ build_and_run_script.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
-mkdir bin;
-g++ src/*.cpp -o bin/dirTree;
+export bindir="bin";
+export srcdir="src";
+export srcfilesext=".cpp";
+export projfilename="dirTree";
+
+command -v g++;
+if [ $? -ne 0 ]; then
+	echo "g++ is not found.";
+	exit $?;
+fi;
+
+if [ ! -d "$srcdir" ]; then
+	echo "$srcdir is not found.";
+	exit -1;
+fi;
+
+if [ ! -d "$bindir" ]; then
+	mkdir "$bindir";
+fi;
+
+g++ "${srcdir}/*${srcfilesext}" -o "${bindir}/${projfilename}";
+
 if [ $? -eq 0 ]; then
-	command bin/dirTree "${@}";
+	command "${bindir}/${projfilename}" "${@}";
 fi;
 
 exit 0;
