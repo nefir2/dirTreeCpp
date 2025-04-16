d09a01a13e0a99e74e4cb1d42d860e8fed982fc8
nefir2 <vadim.ahtamov@gmail.com>
2025-04-16 20:34:50 +0300

    inital files

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+std::string checkArgs(int argc, char** argv, bool &showFiles) {
+	showFiles = false;
+	if (argc < 2 || argc > 3) {
+		std::cerr << "usage: " << argv[0] << " {path} [-f]";
+		exit(-1);
+	}
+	else if (argc == 2) {
+		if (std::string(argv[1]).find("-h")) {
+			std::cout << "usage: " << argv[0] << " {path} [-f]\n\tpath - path to dir start.\n\tf - show files." << std::endl;
+			exit(0);
+		}
+		else return std::string(argv[1]);
+	}
+	else if (argc == 3) {
+		if (std::string(argv[2]).find("-f")) {
+			showFiles = true;
+			return std::string(argv[1]);
+		}
+	} 
+}
+
+int main(int argc, char** argv) {
+	bool showFiles;
+	std::string path {checkArgs(argc, argv, showFiles)};
+	dirTree(path, showFiles);
+	return 0;
+}
