9b344961d380d20eb17797019254e21681e9b0b2
nefir2 <vadim.ahtamov@gmail.com>
2025-04-17 13:47:37 +0300

    less code

--- src/dir_tree.cpp
+++ src/dir_tree.cpp
@@ -7,27 +7,15 @@
 #include "dir_tree.h"
 
 namespace tree {
-
-	void treeFiles(const std::filesystem::path path) {
-		for (std::filesystem::directory_entry de : std::filesystem::recursive_directory_iterator(path)) std::cout << de << std::endl;
-	}
-
-	void treeDirectory(const std::filesystem::path path) {
-		for (std::filesystem::directory_entry de : std::filesystem::recursive_directory_iterator(path)) {
-			if (de.is_directory()) {
-				std::string outp {de.path().string()};
-				//tree::replace(outp, std::string("\\"), std::string("/"));
-				std::replace(outp.begin(), outp.end(), '\\', '/');
-				std::cout << outp << std::endl;
-			}
-		}
-	}
-
 	void dirTree(const std::string path, bool showFiles) {
 		if (!std::filesystem::exists(path)) throw std::invalid_argument("path not exist.");
 		if (!std::filesystem::is_directory(path)) throw std::invalid_argument("path is not directory.");
 		const std::filesystem::path pPath{path};
-		if (showFiles) treeFiles(pPath);
-		else treeDirectory(pPath);
+		for (std::filesystem::directory_entry de : std::filesystem::recursive_directory_iterator(path)) {
+			std::string outp {de.path().string()};
+			std::replace(outp.begin(), outp.end(), '\\', '/');
+			if (de.is_directory()) std::cout << outp << std::endl;
+			else if (!de.is_directory() && showFiles) std::cout << outp << " (" << de.file_size() << "b)" << std::endl;
+		}
 	}
 }
